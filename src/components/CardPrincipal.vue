5863cd873be2d3f6f12db545cb16eddf55b3598c
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-24 17:45:17 -0600

    Modificando componente cardprincipal

--- src/components/CardPrincipal.vue
+++ src/components/CardPrincipal.vue
@@ -1,6 +1,33 @@
-<script setup>
-
-</script>
 <template>
-
+<!-- INICIO DEL PRIMER ROW -->
+    <div class="row">
+      <!-- ¿QUE SON? -->
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header text-center" style="background-color: #fce5b1;">
+              <h6>¿Qué son?</h6>
+          </div>
+          <div class="card-body text-justify">
+            <p class="card-text">Son lenguajes de programación que se utilizan para crear páginas web,
+              trabajan en conjunto para el contenido, considerados lenguajes front-end, es decir, se 
+              ejecutan en el lado del usuario. </p>
+          </div>
+        </div>
+      </div>
+      <!-- FIN -->
+       <!-- FUNCIONES -->
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header text-center" style="background-color: #fce5b1;">
+              <h6>Funciones</h6>
+          </div>
+          <div class="card-body text-justify">
+            <p class="card-text">HTML define la estructura de la pagina web y el contenido, CSS gestiona la presentación, 
+              el diseño y estilo, y JavaScript añade interactividad, son fundamentales para crear páginas web. </p>
+          </div>
+        </div>
+      </div>
+      <!-- FIN -->
+    </div>  
+    <!-- CIERRE DEL PRIMER ROW -->
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,7 @@
 <script setup>
+//importacion del card principal
+import CardPrincipal from './CardPrincipal.vue';
+//importacion del card 1
 import Card from './Card.vue';
 // metodos
 </script>
@@ -9,39 +12,12 @@ import Card from './Card.vue';
     <!-- TITULO -->
     <br><h1 class="text-center">HTML, CSS Y JAVASCRIPT</h1><br>
     <!-- INICIO DEL PRIMER ROW -->
-    <div class="row">
-      <!-- ¿QUE SON? -->
-      <div class="col-md-6">
-        <div class="card">
-          <div class="card-header text-center" style="background-color: #fce5b1;">
-              <h6>¿Qué son?</h6>
-          </div>
-          <div class="card-body text-justify">
-            <p class="card-text">Son lenguajes de programación que se utilizan para crear páginas web,
-              trabajan en conjunto para el contenido, considerados lenguajes front-end, es decir, se 
-              ejecutan en el lado del usuario. </p>
-          </div>
-        </div>
-      </div>
-      <!-- FIN -->
-       <!-- FUNCIONES -->
-      <div class="col-md-6">
-        <div class="card">
-          <div class="card-header text-center" style="background-color: #fce5b1;">
-              <h6>Funciones</h6>
-          </div>
-          <div class="card-body text-justify">
-            <p class="card-text">HTML define la estructura de la pagina web y el contenido, CSS gestiona la presentación, 
-              el diseño y estilo, y JavaScript añade interactividad, son fundamentales para crear páginas web. </p>
-          </div>
-        </div>
-      </div>
-      <!-- FIN -->
-    </div>  
+    <CardPrincipal/>
     <!-- CIERRE DEL PRIMER ROW -->
      <!-- INICIO DEL SEGUNDO ROW -->
       <Card/>
       <!-- CIERRE DEL SEGUNDO ROW -->
+       <Card/>
   </div>
   <!-- CIERRE DEL CONTAINER -->
 
