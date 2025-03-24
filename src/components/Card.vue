fa3a4f52e5e5037a819901855b92deb2d3a814cd
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-24 16:18:56 -0600

    Agregando estructura del proyecto

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,3 +1,40 @@
 <template>
-    
+    <div class="row mt-4">
+        <!-- INICIO DE HTML -->
+        <div class="col-md-4">
+          <div class="card"style="width:18rem;">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/250px-HTML5_logo_and_wordmark.svg.png" 
+            class="card-img-top" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Define la estructura, el diseño y permite que el contenido de las páginas web tenga un formato.</p>
+                <div class="btn btn-warning">Más informacion</div>
+              </div>
+          </div>
+        </div>
+        <!-- FIN DE HTML -->
+        <!-- INICIO DE CSS -->
+        <div class="col-md-4">
+          <div class="card"style="width:18rem;">
+            <img src="https://5.imimg.com/data5/SELLER/Default/2023/12/372222512/FF/CV/GW/64185218/javascript-course-500x500.png" 
+            class="card-img-top" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Permite añadir efectos de estilo dinámicos. </p>
+                <div class="btn btn-warning">Más informacion</div>
+              </div>
+          </div>
+        </div>
+        <!-- FIN DE CSS -->
+         <!-- INICIO DE JS -->
+        <div class="col-md-4">
+          <div class="card"style="width:18rem;">
+            <img src="https://www.jasoft.org/Blog/image.axd?picture=/2017/css3logo.png" 
+            class="card-img-top" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Define el diseño de un sitio web, como los colores, la tipografía, y la disposición de los elementos</p>
+                <div class="btn btn-warning">Más informacion</div>
+              </div>
+          </div>
+        </div>
+        <!-- FIN DE JS -->
+      </div>
 </template>
